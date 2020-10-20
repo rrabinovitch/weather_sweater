15f5b6298c23d923d45ff53e6f9a9456cc632c30
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-10-19 20:43:49 -0600

    Add notes

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
+// requires express and saves it as a variable
 var express = require("express");
+// initializes express server and saves it as a variable
 var app = express();
 
+// set app to listen to port 3000 and create callback function that confirms server is running and on what port
 app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
